620298a1cca1f9f5b06a4c3a4cc4008f99d5c0c3
Joey Vitale <joey.vitale31@gmail.com>
2024-04-01 19:54:44 -0400

    day 1 commit

---
+++ godot/rumble4/scripts/key.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+signal door_opened
+
+var key_taken = false
+var access_door = false
+
