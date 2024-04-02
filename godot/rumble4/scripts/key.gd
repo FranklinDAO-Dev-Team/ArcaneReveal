1c1096b9ac8309e35b03fed8187394ee097ce638
ekqian <eqian.k@gmail.com>
2024-04-02 17:15:35 -0400

    can collect key

--- godot/rumble4/scripts/key.gd
+++ godot/rumble4/scripts/key.gd
@@ -1,16 +1,18 @@
 extends StaticBody2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 signal door_opened
 
 var key_taken = false
-var access_door = false
+var next_to_door = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if key_taken and next_to_door:
+		emit_signal("door_opened")
+
+func _on_area_2d_body_entered(body: CharacterBody2D):
+	if body.name == "Player":
+		print("enter")
+		key_taken = true
+		$Sprite2D.queue_free()
 
