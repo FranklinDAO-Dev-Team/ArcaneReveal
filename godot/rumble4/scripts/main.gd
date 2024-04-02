1659e6843b99961f01444c733568a6ba10fd6702
Jason Gao <jasonzgao@gmail.com>
2024-04-02 12:24:52 -0400

    add out of bounds control and health bar

--- godot/rumble4/scripts/main.gd
+++ godot/rumble4/scripts/main.gd
@@ -1,11 +1,28 @@
-extends StaticBody2D
+extends Node2D
 
+const MAX_HEALTH = 5
+var health = MAX_HEALTH
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _ready() -> void:
+	update_health_ui()
+	$HealthBar.max_value = MAX_HEALTH
 
+func update_health_ui():
+	set_health_label()
+	set_health_bar()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func set_health_label() -> void:
+	$HealthLabel.text = "Health: %s" % health
+
+func set_health_bar() -> void:
+	$HealthBar.value = health
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		damage()
+
+func damage() -> void:
+	health -= 1
+	if health < 0:
+		health = MAX_HEALTH
+	update_health_ui()
--- godot/rumble4/scripts/player.gd
+++ godot/rumble4/scripts/player.gd
@@ -1,36 +1,36 @@
 extends CharacterBody2D
 
-var input_movement = Vector2.ZERO
-var speed = 70
 var currPos = [0, 0]
-
-
-#func _physics_process(delta):
-	#move()
-	#
-#func move():
-	#input_movement = Input.get_vector("left", "right", "up", "down")
-	#
-	#if input_movement != Vector2.ZERO:
-		#velocity = input_movement * speed
-	#
-	#if input_movement == Vector2.ZERO:
-		#velocity = Vector2.ZERO
-		#
-	#move_and_slide()
-
+#
+#
+##func _physics_process(delta):
+	##move()
+	##
+##func move():
+	##input_movement = Input.get_vector("left", "right", "up", "down")
+	##
+	##if input_movement != Vector2.ZERO:
+		##velocity = input_movement * speed
+	##
+	##if input_movement == Vector2.ZERO:
+		##velocity = Vector2.ZERO
+		##
+	##move_and_slide()
+#
 func _input(event):
 	if event.is_action_pressed("right"):
-		if currPos[0] >= 0 and currPos[0] <= 128:
+		if currPos[0] <= 96:
 			currPos[0] += 32
 	elif event.is_action_pressed("left"):
-		if currPos[0] >= 0 and currPos[0] <= 128:
+		if currPos[0] >= 32:
 			currPos[0] -= 32	
 	elif event.is_action_pressed("up"):
-		if currPos[1] >= 0 and currPos[1] <= 128:
+		if currPos[1] >= 32:
 			currPos[1] -= 32
 	elif event.is_action_pressed("down"):
-		if currPos[1] >= 0 and currPos[1] <= 128:	
+		if currPos[1] <= 96:	
 			currPos[1] += 32
 
 	self.position = Vector2(currPos[0], currPos[1])
+
+
