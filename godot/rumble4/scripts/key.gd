37c143767cbdcee9f9a652f33ff85a40d2164ad9
ekqian <eqian.k@gmail.com>
2024-04-02 19:35:33 -0400

    changing key location

--- godot/rumble4/scripts/key.gd
+++ godot/rumble4/scripts/key.gd
@@ -5,6 +5,11 @@ signal door_opened
 var key_taken = false
 var next_to_door = false
 
+const TILE_SIZE = 32
+
+func _ready():
+	self.position = Vector2(TILE_SIZE * 1 - TILE_SIZE / 2, TILE_SIZE * 3 - TILE_SIZE / 2)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if key_taken and next_to_door and Input.is_action_just_pressed("ui_accept"):
