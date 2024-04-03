7868d1747f69f98c1d60976e4b87d6124e7ac60c
ZILECAO <zilecao@sas.upenn.edu>
2024-04-02 21:35:35 -0400

    End of Day 2 full integration reset

--- godot/rumble4/scripts/key.gd
+++ godot/rumble4/scripts/key.gd
@@ -15,16 +15,16 @@ func _process(delta):
 	if key_taken and next_to_door and Input.is_action_just_pressed("ui_accept"):
 		emit_signal("door_opened")
 
-func _on_area_2d_body_entered(body: CharacterBody2D):
+func _on_area_2d_body_entered(body: Area2D):
 	if body.name == "Player" and not key_taken:
 		key_taken = true
 		$Sprite2D.queue_free()
 
-func _on_door_zone_body_entered(body: CharacterBody2D):
+func _on_door_zone_body_entered(body: Area2D):
 	if body.name == "Player":
 		next_to_door = true
 
 
-func _on_door_zone_body_exited(body: CharacterBody2D):
+func _on_door_zone_body_exited(body: Area2D):
 	if body.name == "Player":
 		next_to_door = false
