2e6995b4e1c0885c2c9ee6628b0b4109e0391449
ekqian <eqian.k@gmail.com>
2024-04-03 16:55:00 -0400

    changed health bar to hearts

--- godot/rumble4/scripts/main.gd
+++ godot/rumble4/scripts/main.gd
@@ -4,18 +4,21 @@ const MAX_HEALTH = 5
 var health = MAX_HEALTH
 
 func _ready() -> void:
+	$LifeBar/Life1.play("hearts")
+	$LifeBar/Life2.play("hearts")
+	$LifeBar/Life3.play("hearts")
+	$LifeBar/Life4.play("hearts")
+	$LifeBar/Life5.play("hearts")
 	update_health_ui()
 	$HealthBar.max_value = MAX_HEALTH
 
 func update_health_ui():
-	set_health_label()
 	set_health_bar()
 
-func set_health_label() -> void:
-	$HealthLabel.text = "Health: %s" % health
-
 func set_health_bar() -> void:
-	$HealthBar.value = health
+	for i in range(MAX_HEALTH):
+		$LifeBar.get_child(i).visible = health > i
+		
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
@@ -26,3 +29,4 @@ func damage() -> void:
 	if health < 0:
 		health = MAX_HEALTH
 	update_health_ui()
+	
