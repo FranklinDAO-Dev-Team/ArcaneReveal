7a585cdf79c0d62531d6339d61975536fd3d9e1f
ZILECAO <zilecao@sas.upenn.edu>
2024-04-02 20:01:33 -0400

    added snapping for staff on player 4 directions

--- godot/rumble4/scripts/staff_1.gd
+++ godot/rumble4/scripts/staff_1.gd
@@ -17,17 +17,18 @@ func _process(delta):
 	if draggable:
 		if Input.is_action_just_pressed("click"):
 			offset = get_global_mouse_position() - global_position
+
 			global.is_dragging = true
 		if Input.is_action_pressed("click"):
 			global_position = get_global_mouse_position() - offset
-			
-		elif Input.is_action_just_pressed("click"):
+		
+		elif Input.is_action_just_released("click"):
 			global.is_dragging = false
 			var tween = get_tree().create_tween()
 			if is_inside_dropable:
 				tween.tween_property(self, "position", body_ref.position, 0.2).set_ease(Tween.EASE_OUT)
 			else:
-				tween.tween_property(self, "global_position", initialPos, 0.2).set_ease(Tween.EASE_OUT)
+				tween.tween_property(self, "global_position", global_position, 0.2).set_ease(Tween.EASE_OUT)
 				
 
 func _on_area_2d_mouse_entered(): 
