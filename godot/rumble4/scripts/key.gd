b8c655bd57a71868129594d1c2171b58b09d46f1
ekqian <eqian.k@gmail.com>
2024-04-03 12:11:50 -0400

    fixed key and door bug

--- godot/rumble4/scripts/key.gd
+++ godot/rumble4/scripts/key.gd
@@ -15,16 +15,16 @@ func _process(delta):
 	if key_taken and next_to_door and Input.is_action_just_pressed("ui_accept"):
 		emit_signal("door_opened")
 
-func _on_area_2d_body_entered(body: Area2D):
-	if body.name == "Player" and not key_taken:
+
+func _on_area_2d_area_entered(area):
+	if area.name == "Player" and not key_taken:
 		key_taken = true
 		$Sprite2D.queue_free()
 
-func _on_door_zone_body_entered(body: Area2D):
-	if body.name == "Player":
-		next_to_door = true
+
+func _on_door_entered_door_zone():
+	next_to_door = true
 
 
-func _on_door_zone_body_exited(body: Area2D):
-	if body.name == "Player":
-		next_to_door = false
+func _on_door_exited_door_zone():
+	next_to_door = false
