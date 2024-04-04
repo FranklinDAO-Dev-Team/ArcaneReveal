5761a6e9d335ba046e0808679cbf807c73067dd5
ekqian <eqian.k@gmail.com>
2024-04-04 17:36:45 -0400

    joey's changes

--- godot/rumble4/scripts/enemyV3.gd
+++ godot/rumble4/scripts/enemyV3.gd
@@ -3,8 +3,6 @@ extends Area2D
 var animation_speed = 4
 var moving = false
 var tile_size = 64
-const max_health = 5
-var health = max_health
 var inputs = {
 	"enemy_right": Vector2.RIGHT,
 	"enemy_left": Vector2.LEFT,
@@ -13,30 +11,15 @@ var inputs = {
 }
 
 @onready var ray = $RayCast2D
-@onready var health_bar = $healthbar
 
 func _ready():
-	update_health()
 	position = position.snapped(Vector2.ONE * tile_size)
-	
-	
-func update_health():
-	var healthbar = $healthbar  
-	healthbar.value = health
-	if health < max_health:
-		healthbar.visible = false
-	else:
-		healthbar.visible = true
-		
-func damage() -> void:
-	health -= 1
-	if health < 0:
-		health = max_health
-	
-	
+	#position += Vector2.ONE * tile_size / 
+
 func _process(delta):
 	$Sprite.play("idle")
 		
+
 func _unhandled_input(event):
 	if moving:
 		return
@@ -58,13 +41,4 @@ func move(dir):
 
 func _on_area_entered(area):
 	if area.name == "Player":
-		area.damage()
-		
-func _physics_process(delta):
-	update_health()
-	
-func take_damage() -> void:
-	health -= 1
-	if health < 0:
-		health = max_health
-	#update_health_ui()
+		area.damage_heavy()
