63ca850464cb2e8eccd650062ccedbe1d55f20e1
ZILECAO <zilecao@sas.upenn.edu>
2024-04-05 11:52:49 -0400

    connected nakjama scoket on zile laptop

--- godot/rumble4/scripts/NakamaClient.gd
+++ godot/rumble4/scripts/NakamaClient.gd
@@ -5,10 +5,6 @@ extends Node
 
 var enemies_defeated = 0
 
-func _ready():
-	enemy1.tree_exited.connect(_on_enemy_exited.bind(enemy1))
-	enemy2.tree_exited.connect(_on_enemy_exited.bind(enemy2))
-
 func _on_enemy_exited(enemy: Area2D):
 	if not enemy.is_inside_tree():
 		enemies_defeated += 1
@@ -16,32 +12,33 @@ func _on_enemy_exited(enemy: Area2D):
 			# Display WIN text or perform any other desired action
 			$GameWinLabel.visible = true 
 
-#
-#@onready var client : NakamaClient = Nakama.create_client("defaultkey", "shiny-pens-wave.loca.lt", 443, "https")
-#@onready var socket = Nakama.create_socket_from(client)
-#
-#func _ready():
-#
-	## Get the System's unique device identifier
-	#var device_id = OS.get_unique_id()
-	#print('test')
-#
-	## Authenticate with the Nakama server using Device Authentication
-	#var session : NakamaSession = await client.authenticate_device_async(device_id)
-	#if session.is_exception():
-		#print("An error occurred: %s" % session)
-		#return
-	#print("Successfully authenticated: %s" % session)
-#
-	#var connected : NakamaAsyncResult = await socket.connect_async(session)
-	#if connected.is_exception():
-		#print("An error occurred: %s" % connected)
-		#return
-	#print("Socket connected.")
-	#
-	#socket.received_notification.connect(self._on_notification)
-#
-#func _on_notification(p_notification : NakamaAPI.ApiNotification):
-	#var notification = JSON.new()
-	#notification.parse(p_notification.content)
-	#print(notification.data)
+
+@onready var client : NakamaClient = Nakama.create_client("defaultkey", "127.0.0.1", 7350, "http")
+@onready var socket = Nakama.create_socket_from(client)
+
+func _ready():
+	enemy1.tree_exited.connect(_on_enemy_exited.bind(enemy1))
+	enemy2.tree_exited.connect(_on_enemy_exited.bind(enemy2))
+	# Get the System's unique device identifier
+	var device_id = OS.get_unique_id()
+	print('test')
+
+	# Authenticate with the Nakama server using Device Authentication
+	var session : NakamaSession = await client.authenticate_device_async(device_id)
+	if session.is_exception():
+		print("An error occurred: %s" % session)
+		return
+	print("Successfully authenticated: %s" % session)
+
+	var connected : NakamaAsyncResult = await socket.connect_async(session)
+	if connected.is_exception():
+		print("An error occurred: %s" % connected)
+		return
+	print("Socket connected.")
+	
+	socket.received_notification.connect(self._on_notification)
+
+func _on_notification(p_notification : NakamaAPI.ApiNotification):
+	var notification = JSON.new()
+	notification.parse(p_notification.content)
+	print(notification.data)
